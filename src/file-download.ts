6a25d6953277379ac2a1581466805bcad448edf1
Zenith Wogwugwu <uzenith360@gmail.com>
2023-02-15 18:04:29 +0100

    fix: update mode to cors

--- src/file-download.ts
+++ src/file-download.ts
@@ -3,14 +3,24 @@
 
 export default async (url: string, fileName: string)
   : Promise<void> => {
-    const image = await fetch(url);
-    const imageBlog = await image.blob();
-    const imageURL = URL.createObjectURL(imageBlog);
-  
-    const anchor = document.createElement("a");
-    anchor.href = imageURL;
-    anchor.download = fileName;
-  
-    document.body.appendChild(anchor);
-    anchor.click();
+  const res = await fetch(
+    url,
+    {
+      method: 'get',
+      mode: 'no-cors',
+      referrerPolicy: 'no-referrer',
+    },
+  );
+
+  const res_1 = await res.blob();
+  const aElement = document.createElement('a');
+  aElement.setAttribute('download', fileName);
+
+  const href = URL.createObjectURL(res_1);
+  aElement.href = href;
+  // aElement.setAttribute('href', href);
+  aElement.setAttribute('target', '_blank');
+  aElement.click();
+
+  URL.revokeObjectURL(href);
 };
